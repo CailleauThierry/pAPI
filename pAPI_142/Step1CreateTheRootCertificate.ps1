c18c86b605a1171b326773b0c8f7ea3085783d93
CailleauThierry <thierrycailleau@yahoo.fr>
2019-08-28 08:48:18 +0200

    renaming

--- pAPI_142/Step1CreateTheRootCertificate.ps1
+++ pAPI_142/Step1CreateTheRootCertificate.ps1
@@ -1,5 +1,5 @@
 ﻿#The following script is based on the info from https://infiniteloop.io/powershell-self-signed-certificate-via-self-signed-root-ca/
-#Replace the DnsName "rs-2k16.corp.ssv.com" with the FQDN of the server you want to create the certificate for
+#Replace the DnsName "SRVpAPI.yourdomain.com" with the FQDN of the server you want to create the certificate for
 #Replace the DnsName "My Root CA" with whichever name you wish to use as the Root CA
 #The password for the certificate is 3Vlt1nc.  This can be changed to whatever password you like
 #Step 1 - Create the root certificate
@@ -17,7 +17,7 @@ $rootCA = New-SelfSignedCertificate @params
 
 #Step 2 - Create the server cert signed by the new root
 $params = @{
-  DnsName = "rs-2k16.corp.ssv.com"
+  DnsName = "SRVpAPI.yourdomain.com"
   Signer = $rootCA
   KeyLength = 2048
   KeyAlgorithm = 'RSA'
